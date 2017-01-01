02918c4978dde13e701f18938e147f425fea6520
rahim373 <me.rahim29>
2017-01-01 10:10:59 +0600

    Complete V1.0 For Showcase

--- FavMeal.Web/Controllers/CommentsController.cs
+++ FavMeal.Web/Controllers/CommentsController.cs
@@ -7,19 +7,17 @@ using Microsoft.AspNet.Identity;
 
 namespace FavMeal.Web.Controllers
 {
-    [Authorize]
     public class CommentsController : Controller
     {
-        readonly CommentService service = new CommentService();
+        readonly CommentService _service = new CommentService();
 
         // GET: Comments
-        [HttpPost]
         public ActionResult Save(Comment comment)
         {
 
             comment.DateTime = DateTime.UtcNow;
             comment.ReviewBy = User.Identity.GetUserId();
-            if (service.Save(comment)) return new HttpStatusCodeResult(HttpStatusCode.OK);
+            if (_service.Save(comment)) return new HttpStatusCodeResult(HttpStatusCode.OK);
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
     }
--- FavMealService/Migrations/Configuration.cs
+++ FavMealService/Migrations/Configuration.cs
@@ -31,6 +31,21 @@ namespace FavMealService.Migrations
             context.Categories.AddOrUpdate(x=> x.Name, 
                 new Category {Name = "Fastfood"},
                 new Category {Name = "Drinks"},
+                new Category {Name = "Crusher"},
+                new Category {Name = "Set Menu"},
+                new Category {Name = "Desi"},
+                new Category {Name = "Biriyani"},
+                new Category {Name = "Street Food"},
+                new Category {Name = "Japanese"},
+                new Category {Name = "Thai"},
+                new Category {Name = "Mexican"},
+                new Category {Name = "Icecream"},
+                new Category {Name = "Indian"},
+                new Category {Name = "Bakery"},
+                new Category {Name = "Korean"},
+                new Category {Name = "Sea Food"},
+                new Category {Name = "Burger"},
+                new Category {Name = "Fastfood"},
                 new Category {Name = "Chinese"}
                 );
         }
